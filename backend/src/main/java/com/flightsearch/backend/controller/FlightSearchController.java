ac612855706f6891557ec00971ddbe84d549df18
abraham-villalba <abraham.villalba@encora.com>
2025-01-28 17:18:23 -0700

    Added basic Request DTO for flight search and controller

--- backend/src/main/java/com/flightsearch/backend/controller/FlightSearchController.java
+++ backend/src/main/java/com/flightsearch/backend/controller/FlightSearchController.java
@@ -1,5 +1,35 @@
 package com.flightsearch.backend.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.flightsearch.backend.model.DTO.FlightSearchRequestDTO;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/flights")
 public class FlightSearchController {
 
+    @GetMapping("/search")
+    public String searchFlights(
+        @RequestParam String departureCode,
+        @RequestParam String destinationCode,
+        @RequestParam String departureDate,
+        @RequestParam(required = false) String returnDate,
+        @RequestParam Integer numAdults,
+        @RequestParam Boolean nonStop
+    ) {
+        LocalDate departureLocalDate = LocalDate.parse(departureDate, DateTimeFormatter.ISO_DATE);
+        
+        System.out.println("Departure Date: " + departureLocalDate.toString());
+        FlightSearchRequestDTO request = new FlightSearchRequestDTO(departureCode, destinationCode, departureLocalDate, null, numAdults, nonStop);
+        return new String("Hello World");
+    }
+    
 }
---
+++ backend/src/main/java/com/flightsearch/backend/model/DTO/FlightSearchRequestDTO.java
@@ -0,0 +1,46 @@
+package com.flightsearch.backend.model.DTO;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import io.micrometer.common.lang.Nullable;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FlightSearchRequestDTO {
+
+    @NotNull
+    @Size(min = 3, max = 3, message = "Departure code must be exactly 3 characters long. Eg: LAX")
+    private String departureCode;
+
+    @NotNull
+    @Size(min = 3, max = 3, message = "Destination code must be exactly 3 characters long. Eg: LAX")
+    private String destinationCode;
+
+    @NotNull(message = "Departure date must not be null")
+    @FutureOrPresent
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate departureDate;
+    
+    @Nullable
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate returnDate;
+
+    @NotNull(message = "numAdults can't be null")
+    @Max(value=9, message = "Maximum number of adults is 9")
+    private Integer numAdults;
+
+    @NotNull(message = "nonStop can't be null")
+    private Boolean nonStop;
+}
