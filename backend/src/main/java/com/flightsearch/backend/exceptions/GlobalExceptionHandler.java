de4ddd857936fe442dd3d7822e36f5e7a8c94f71
abraham-villalba <abraham.villalba@encora.com>
2025-01-28 17:28:42 -0700

    Base GlobalExceptionHandler added

---
+++ backend/src/main/java/com/flightsearch/backend/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.flightsearch.backend.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus()
+    public Map<String,Object> handleGeneralException(Exception e) {
+        Map<String,Object> errors = new HashMap<>();
+        errors.put("error", "Internal Server Error: Something went wrong...\n" + e.getMessage());
+        return errors;
+    }
+
+}
